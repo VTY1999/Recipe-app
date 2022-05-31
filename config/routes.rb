06a0a319239edc4093901b3f6a13c2d07a0fe0ac
kandy-peter <kandypeter03@gmail.com>
2022-05-31 19:54:39 +0200

    Create routes for each path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'home#index'
+
+  resources :foods, only: [:index, :show, :new, :create]
+  resources :recipes, only: [:index, :show, :new] do
+    resources :recipe_foods, only: [:new, :create]
+  end
+  resources :public_recipes, only: [:public_recipes]
 end
