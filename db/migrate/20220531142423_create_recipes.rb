e8b00e12046e9bc2ea4af8cb14ea8ec647c5a891
kandy-peter <kandypeter03@gmail.com>
2022-05-31 16:42:49 +0200

    Generate tables and migrations

---
+++ db/migrate/20220531142423_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.references :user, index:true, foreign_key: {to_table: :users}
+      t.string :name, null: false
+      t.integer :preparation_time
+      t.integer :cooking_time
+      t.string :description, null: false
+      t.boolean :public
+
+      t.timestamps
+    end
+  end
+end
