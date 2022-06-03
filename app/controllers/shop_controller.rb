0c59a67ee700231529d590e01836e8cadfee1082
kandy-peter <kandypeter03@gmail.com>
2022-06-03 18:31:43 +0200

    Fix linters errors

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -3,7 +3,7 @@ class ShopController < ApplicationController
     @foods = Food.all
     @recipe_food = RecipeFood.where(recipe_id: params[:recipe_id]).includes(:food)
     food = Food.where(user: current_user)
-    list_recipe_food = @recipe_food.where.not(food: {name: food.name})
+    list_recipe_food = @recipe_food.where.not(food: { name: food.name })
 
     @total_amount = list_recipe_food.count
   end
